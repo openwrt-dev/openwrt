844b05a69bb44be62751cb5a978796ade00a321a
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2019-12-08 20:18:58 +0100

    ath79: add support for GL.iNet 6408/6416 (GL.iNet V1)
    This ports the GL.iNet 6408/6416 from ar71xx.
    
    The GL-Connect GL.iNet v1 routers are basically a TP-Link TL-WR710N with
    more DRAM/Flash and console/GPIO header in the same small form-factor.
    
    Specifications:
     - SoC: Atheros AR9331
     - CPU: 400 MHz
     - Flash: 8/16 MiB
     - RAM: 64 MiB
     - WiFi: 2.4 GHz b/g/n (SoC)
     - Ethernet: 2x 100M ports (LAN/WAN)
     - USB: 1x 2.0
    
    The difference between 6408 and 6416 is just the flash size. It looks like
    only the 16 MiB version has been advertised, while the 6408 is a modified
    version. There are also 1-port versions sold by third parties.
    
    Installation:
    Install the sysupgrade image via stock firmware GUI or upload it via uboot
    (web-based). The device will be available at 192.168.1.1.
    
    Attention: In ar71xx, the same board name is used for both flash versions.
    So, please make sure you flash the correct ath79 image when upgrading.
    
    This has been device-tested on a GL.iNet 6416.
    
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- target/linux/ath79/image/generic.mk
+++ target/linux/ath79/image/generic.mk
@@ -369,6 +369,30 @@ define Device/etactica_eg200
 endef
 TARGET_DEVICES += etactica_eg200
 
+define Device/glinet_6408
+  $(Device/tplink-8mlzma)
+  ATH_SOC := ar9331
+  DEVICE_TITLE := GL.iNet 6408
+  DEVICE_PACKAGES := kmod-usb2
+  IMAGE_SIZE := 8000k
+  TPLINK_HWID := 0x08000001
+  IMAGES := sysupgrade.bin
+  SUPPORTED_DEVICES += gl-inet
+endef
+TARGET_DEVICES += glinet_6408
+
+define Device/glinet_6416
+  $(Device/tplink-16mlzma)
+  ATH_SOC := ar9331
+  DEVICE_TITLE := GL.iNet 6416
+  DEVICE_PACKAGES := kmod-usb2
+  IMAGE_SIZE := 16192k
+  TPLINK_HWID := 0x08000001
+  IMAGES := sysupgrade.bin
+  SUPPORTED_DEVICES += gl-inet
+endef
+TARGET_DEVICES += glinet_6416
+
 define Device/glinet_gl-ar150
   ATH_SOC := ar9330
   DEVICE_TITLE := GL.iNet GL-AR150
