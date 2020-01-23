27040adeeb15548c4327ce396fa010c224aa26a8
pexcn <i@pexcn.me>
2020-01-23 19:07:55 +0800

    support for specify kernel vermagic

--- include/kernel-defaults.mk
+++ include/kernel-defaults.mk
@@ -105,7 +105,9 @@ define Kernel/Configure/Default
 		cp $(LINUX_DIR)/.config.set $(LINUX_DIR)/.config.prev; \
 	}
 	$(_SINGLE) [ -d $(LINUX_DIR)/user_headers ] || $(KERNEL_MAKE) INSTALL_HDR_PATH=$(LINUX_DIR)/user_headers headers_install
-	grep '=[ym]' $(LINUX_DIR)/.config.set | LC_ALL=C sort | mkhash md5 > $(LINUX_DIR)/.vermagic
+	VERMAGIC=$(call qstrip,$(CONFIG_KERNEL_VERMAGIC)); \
+	[ -n "$$$$VERMAGIC" ] && echo $$$$VERMAGIC > $(LINUX_DIR)/.vermagic || \
+		grep '=[ym]' $(LINUX_DIR)/.config.set | LC_ALL=C sort | mkhash md5 > $(LINUX_DIR)/.vermagic
 endef
 
 define Kernel/Configure/Initramfs
